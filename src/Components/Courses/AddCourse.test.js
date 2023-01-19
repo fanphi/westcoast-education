2743504c82e93030c60db3ad594b12d8bb934daf
fanphi <fannyp90@hotmail.com>
2023-01-19 15:11:24 +0100

    added test to AddCourse

---
+++ src/Components/Courses/AddCourse.test.js
@@ -0,0 +1,20 @@
+import { screen, render } from "@testing-library/react";
+import AddCourse from "./AddCourse";
+
+describe("AddCourse component", () => {
+  const setup = () => render(<AddCourse />);
+
+  describe("AddCourse page", () => {
+    test('has a "Kursnamn" input', () => {
+      // Arrange
+      setup();
+      const courseNameInput = screen.getByLabelText("Kursnamn");
+
+      // Act
+
+      // Assert
+      expect(courseNameInput).toBeInTheDocument();
+    });
+  });
+
+});
